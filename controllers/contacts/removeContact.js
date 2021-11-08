d65e4dbae321e8471be1a65ded096f3051e67998
funnykoss <75586141+funnykoss@users.noreply.github.com>
2021-11-09 00:31:11 +0200

    remuveContacts_update

--- controllers/contacts/removeContact.js
+++ controllers/contacts/removeContact.js
@@ -1,9 +1,10 @@
-const contactsOperations = require("../../model/contact");
+const { Contact } = require("../../models");
+
 const { NotFound } = require("http-errors");
 
 const removeContact = async (req, res) => {
   const { contactId } = req.params;
-  const result = await contactsOperations.removeContact(contactId);
+  const result = await Contact.findByIdAndRemove(contactId);
   if (!result) {
     throw new NotFound(`Contact with id=${contactId} is not found`);
   }
