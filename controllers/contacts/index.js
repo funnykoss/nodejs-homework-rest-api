cb7f7b810f5cb74cf0fbd7bf4a7dbe01a99dddce
funnykoss <75586141+funnykoss@users.noreply.github.com>
2021-11-09 00:30:44 +0200

    update ListContacts

--- controllers/contacts/index.js
+++ controllers/contacts/index.js
@@ -1,13 +1,15 @@
-// const listContacts = require("./listContacts");
-// const getContactById = require("./getContactById");
+const listContacts = require("./listContacts");
+const getContactById = require("./getContactById");
 const addContact = require("./addContact");
-// const updateById = require("./updateById");
-// const removeContact = require("./removeContact");
+const updateById = require("./updateById");
+const removeContact = require("./removeContact");
+const updateStatusContact = require("./updateStatusContact");
 
 module.exports = {
-  // listContacts,
-  // getContactById,
+  listContacts,
+  getContactById,
   addContact,
-  // updateById,
-  // removeContact,
+  updateById,
+  removeContact,
+  updateStatusContact,
 };
